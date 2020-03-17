2c0cfe12ab11a542323dce63684cd42ffc4905e8
Bobby Roe <rroe@seattlecca.org>
2020-03-17 13:03:31 -0700

    checkboxes

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-// Expander toggle
-$('.expander-toggle').click(function() {
-  if ($(this).attr('aria-expanded') == 'false') {
-    $(this)
-      .attr('aria-expanded', 'true')
-      .next('.descriptive-text')
-      .addClass('visible')
-      .attr('aria-hidden', 'false');
-  } else if ($(this).attr('aria-expanded') == 'true') {
-    $(this)
-      .attr('aria-expanded', 'false')
-      .next('.descriptive-text')
-      .removeClass('visible')
-      .attr('aria-hidden', 'true');
+const allCriteria = {
+  'alt-tags': {
+    id: 111,
+    md:
+      '- Write good alt text for your images [More Info](https://accessibility.voxmedia.com/#about#engineers-4)'
+  },
+  'keyboard-nav': {
+    id: 999,
+    md:
+      ' - Support keyboard navigation [More Info](https://accessibility.voxmedia.com/#about#engineers-2)'
+  },
+  'html-landmarks': {
+    id: 9999,
+    md:
+      '- Understand and use HTML landmarks [More Info](https://accessibility.voxmedia.com/#about#engineers-3)'
   }
-});
+};
 
-$("[name='expanderList']").on('change', function() {
-  $("[name='expanderList']")
-    .not(this)
-    .prop('checked', false);
+function findCriteria(id) {
+  return id;
+}
+
+document.querySelector('body').addEventListener('click', evt => {
+  if (evt.target.type === 'checkbox') {
+    const id = evt.target.id;
+    console.log(id, evt.target.type);
+    const criteria = allCriteria[findCriteria(id)];
+    console.log(criteria.md);
+  } else {
+    console.log('other');
+  }
 });
