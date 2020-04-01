324ab8356de6feb5c515a046040527f3b1aeeb0a
Bobby Roe <rroe@seattlecca.org>
2020-04-01 14:18:41 -0700

    updated criteria

--- data/criteria.js
+++ data/criteria.js
@@ -6,10 +6,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Text Alternatives',
     level: 'A',
+    markdown: ' - Write good alt text for your images',
     DRRSFA: 'Description',
     '*thing*': 'Image',
     '[more info]()': 'https://a11yengineer.com/thing/image',
-    markdown: ' - Write good alt text for your images',
     notes:
       'Users with low vision often make use of a talking browser to “read” the web. These specialized browsers convert text to speech so that a user can hear the words on a site. When a talking browser lands on an image, it looks for alt text that it can read aloud; if it finds none, it will often just say “image,” leaving the user in the dark as to what the image is and how it matters to the story.  Describe all of the elements that explain what’s happening in the image, rather than just setting the alt text to be something like, “photograph"  If you have to use an image of text, be sure to describe the design if relevant, as well as all of the words in the image. (Ex: Whiteboard drawing of the quote “This is a quote”)',
     'addl. info': ''
@@ -21,10 +21,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Text Alternatives',
     level: 'A',
+    markdown: ' - Hide decorative elements from screen readers ',
     DRRSFA: 'Description',
     '*thing*': 'Image',
     '[more info]()': 'https://a11yengineer.com/thing/image',
-    markdown: ' - Hide decorative elements from screen readers ',
     notes:
       'Decorative elements, such as dividing lines, pull-quotes, or non-informative icons, can be hidden from screen readers to ensure that a user only receives relevant information as they peruse the page. This is done by adding aria-hidden=”true”  to the element.',
     'addl. info': '"decorative" images have empty alt text'
@@ -36,11 +36,12 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Text Alternatives',
     level: 'A',
+    markdown: ' - Buttons have a descriptive value',
     DRRSFA: 'Description',
     '*thing*': 'Button',
     '[more info]()': 'https://a11yengineer.com/thing/button',
-    markdown: ' - Form buttons have a descriptive value',
-    notes: 'TBD',
+    notes:
+      '"Click here," "More Info," or "Go" are examples of not enough text to describe a button\'s function. Use just enough words to provide context for assistive technology (like a screen reader).',
     'addl. info': ''
   },
   {
@@ -50,11 +51,12 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Text Alternatives',
     level: 'A',
+    markdown: ' - Inputs have good labels',
     DRRSFA: 'Description',
     '*thing*': 'Input',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Form inputs have good labels',
-    notes: 'TBD',
+    '[more info]()': '',
+    notes:
+      'An input control like a text field or a checkbox without associated label will be a confusing and mysterious experience for a assistive technology.',
     'addl. info': 'How does the screen reader read these?'
   },
   {
@@ -64,10 +66,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Adaptable',
     level: 'A',
+    markdown: ' - Use the correct HTML element for your content',
     DRRSFA: 'Role',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Use the correct HTML element for your content',
+    '[more info]()': '',
     notes:
       'HTML elements communicate to the browser what kind of content they contain and how the browser should render or treat that content. The accessibility tree is built off of assumptions about the elements and their structure. This is the browser feature which powers screen readers. Choosing the correct element for the current context is a simple way to create a good foundation for your experiences.',
     'addl. info': ''
@@ -79,10 +81,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Adaptable',
     level: 'A',
+    markdown: ' - Understand and use HTML landmarks ',
     DRRSFA: 'Focus, Action',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Understand and use HTML landmarks ',
+    '[more info]()': '',
     notes:
       'People who use assistive devices may not want to peruse your content linearly, instead preferring to use an outline view (generated from markup) to find the relevant content for their current needs. These landmarks, like main and form and navigation help define specific areas that a user might want to seek out and can save them a lot of time.',
     'addl. info': ''
@@ -94,10 +96,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Adaptable',
     level: 'A',
+    markdown: ' - Content and Nav are in logical order',
     DRRSFA: 'Keyboard',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'When a customer tabs through the page content, they will rely on the context and structure to understand where they are and where to go for what they seek.',
     'addl. info': 'also 2.4.3 : Operable : Navigable'
@@ -109,10 +111,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Adaptable',
     level: 'A',
+    markdown: ' - Use position, labeling and color to identify importance',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'You should not rely solely on images, shape, size, visual location, orientation, or sound to indicate important instructions for operating or understanding content (ex. “See the image above”). ',
     'addl. info': ''
@@ -124,10 +126,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Distinguishable',
     level: 'A',
+    markdown: ' - Use more than color to identify importance',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'There should be another indicator (such as icons to accompany color coding, or an underline on linked text) so that people who cannot easily distinguish colors will be able to understand and use your content.',
     'addl. info': ''
@@ -139,10 +141,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Distinguishable',
     level: 'A',
+    markdown: ' - Audio and video must be pausible.',
     DRRSFA: '',
     '*thing*': 'Video',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'A mechanism is provided to stop, pause, mute, or adjust volume for audio that automatically plays on a page for more than 3 seconds.',
     'addl. info': ''
@@ -154,10 +156,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Distinguishable',
     level: 'A',
+    markdown: ' - Make sure there is enough contrast between text and its background color',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'According to the WCAG, the contrast ratio between text and the text’s background should be at least 4.5 to 1. If your font is at least 24 px or 19 px bold, the minimum drops to 3 to 1 (though, worth noting that this is a little fuzzy because numbered sizes aren’t always reflective of the visual size type). Be especially careful of text over images.',
     'addl. info': ''
@@ -169,10 +171,10 @@ const allCriteria = [
     category: 'Perceivable',
     subcategory: 'Distinguishable',
     level: 'AAA',
+    markdown: ' - Pair values of colors together (not only hues) to increase contrast ',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'When you strip out the color information from two hues and compare the values, there may not be much difference. Pairing different values creates contrast.',
     'addl. info': ''
@@ -184,10 +186,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Keyboard Accessible',
     level: 'A',
+    markdown: ' - Support and test keyboard navigation',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Support keyboard navigation',
+    '[more info]()': '',
     notes:
       'Browsers support tabbing through link, form, button elements. This is an easy way to move around the page, but it’s very easy to accidentally hinder this functionality: by relying on CSS to move elements around instead of actually reordering the HTML; hiding elements from tab flow, either by faking buttons (with JavaScript) in lieu of using button elements or disabling tab via tabindex=”-1”; and hiding form elements (on a tabbed interface) but not removing them from the tab flow (so that hidden form elements can be focused).',
     'addl. info': ''
@@ -199,10 +201,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Keyboard Accessible',
     level: 'A',
+    markdown: ' - Users should be able to navigate through content using their keyboard.',
     DRRSFA: 'Keyboard',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'Keyboard focus is never locked or trapped at one particular page element. The user can navigate to and from all navigable page elements using only a keyboard.',
     'addl. info': ''
@@ -215,10 +217,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Enough Time',
     level: 'A',
+    markdown: ' - Allow customers to extend time limits',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'If a page or application has a time limit, the user is given options to turn off, adjust, or extend that time limit. ',
     'addl. info': ''
@@ -231,10 +233,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Enough Time',
     level: 'A',
+    markdown: ' - Animations lasting 5 seconds or more are pausable.',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'Automatically updating content (e.g., a dynamically-updating news ticker, chat messages, etc.) can be paused, stopped, or hidden by the user or the user can manually control the timing of the updates.',
     'addl. info': ''
@@ -246,10 +248,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Navigable',
     level: 'A',
+    markdown: ' - Give users a way to skip top level navigation to access main content',
     DRRSFA: 'Keyboard',
     '*thing*': 'Link',
     '[more info]()': 'https://a11yengineer.com/thing/link',
-    markdown: ' - Give users a way to skip top level navigation to access main content',
     notes:
       'For keyboard users, it can be helpful to give them a way to skip past top level navigation to get to the main content. This is typically a button at the top of the page (it can only be visible if active), that, when clicked, shifts the current focus from the button to the top of the main content of the page. This saves the user from having to tab through many many elements of navigation.',
     'addl. info': ''
@@ -261,10 +263,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Navigable',
     level: 'A',
+    markdown: ' - Make links descriptive',
     DRRSFA: 'Description',
     '*thing*': 'Link',
     '[more info]()': 'https://a11yengineer.com/thing/link',
-    markdown: ' - Make links descriptive',
     notes:
       'Assistive technology is able to find all links on a page and present them in various forms, but these links are rather useless if it’s a long list of links that are just the text “click here.” A better way is to have the link describe where the user will go if they click it, giving them an idea of what’s on the other side of clicking.',
     'addl. info': ''
@@ -276,10 +278,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Navigable',
     level: 'A',
+    markdown: ' - Design focus states to help users navigate and understand where they are',
     DRRSFA: 'Focus',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Design focus states to help users navigate and understand where they are',
+    '[more info]()': '',
     notes:
       'Your designs should never actively hide focus states. When people use the keyboard to navigate, your product should include highly visible focus states.',
     'addl. info': ''
@@ -291,10 +293,10 @@ const allCriteria = [
     category: 'Operable',
     subcategory: 'Navigable',
     level: 'A',
+    markdown: ' - Links should be visually identifiable and have clear :focus and :active states',
     DRRSFA: 'Focus',
     '*thing*': 'Link',
     '[more info]()': 'https://a11yengineer.com/thing/link',
-    markdown: ' - Links should be visually identifiable and have clear :focus and :active states',
     notes:
       "The outline property indicates when an element or selected or has focus. This is helpful to users who don't have the ability to use a mouse or are visually impaired.",
     'addl. info': '** Duplicate?'
@@ -306,10 +308,10 @@ const allCriteria = [
     category: 'Understandable',
     subcategory: 'Readable',
     level: 'A',
+    markdown: ' - HTML document should have a language attribute',
     DRRSFA: 'Description',
     '*thing*': 'HTML',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - HTML document should have a language attribute',
+    '[more info]()': '',
     notes:
       'The HTML lang attribute is used to identify the language of text content on the web. This information helps search engines return language specific results, and it is also used by screen readers that switch language profiles to provide the correct accent and pronunciation. e.g. <html lang="en">',
     'addl. info': ''
@@ -321,10 +323,10 @@ const allCriteria = [
     category: 'Understandable',
     subcategory: 'Readable',
     level: 'AAA',
+    markdown: ' - Be as consistent and clear as possible in layout and copy',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Ipsum lorem dolar molar growler grunt',
+    '[more info]()': '',
     notes:
       'Be consistent across functions, placement, and labeling. Components with the same functionality should work and be identified consistently. Use section headings to organize content. Be clear in writing; avoid jargon and idioms',
     'addl. info': ''
@@ -336,10 +338,10 @@ const allCriteria = [
     category: 'Understandable',
     subcategory: 'Input Assist',
     level: 'A',
+    markdown: ' - Help users understand inputs, and help them avoid and correct mistakes',
     DRRSFA: 'Description',
     '*thing*': 'Input',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Help users understand inputs, and help them avoid and correct mistakes',
+    '[more info]()': '',
     notes:
       'Labels should never completely go away when the focus is inside of an input. Users should always have clear instructions on what they should be inputting. Put error messages in text that explain the error and how to fix the error. Never rely solely on color to indicate errors.',
     'addl. info': 'also: 3.3.2 and 3.3.3 (AA)'
@@ -351,10 +353,10 @@ const allCriteria = [
     category: 'Robust',
     subcategory: 'Compatible',
     level: 'A',
+    markdown: ' - Use ARIA attributes when applicable',
     DRRSFA: 'DRRS**',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Use ARIA attributes when applicable',
+    '[more info]()': '',
     notes:
       'ARIA stands for Accessible Rich Internet Applications. It is an optional but helpful spec to define ways to markup HTML that has dynamic experiences (typically controlled by JavaScript).  The goal when using them is to communicate to the browser and assistive technology how content is going to change or what the purpose of the content is. Dynamic error messages can be called out to screenreaders, buttons can be linked to the content they affect and many other helpful states can be expressed on a code level.',
     'addl. info': 'The 1st rule of ARIA is: "Don\'t use ARIA" – use a semantic HTML tag instead'
@@ -366,10 +368,10 @@ const allCriteria = [
     category: 'Robust',
     subcategory: 'Compatible',
     level: 'A',
+    markdown: ' - Avoid images and iconography in pseudo-elements',
     DRRSFA: 'Focus',
     '*thing*': 'Image',
     '[more info]()': 'https://a11yengineer.com/thing/image',
-    markdown: ' - Avoid images and iconography in pseudo-elements',
     notes:
       'Assistive technology relies on HTML to find the content to present to users. It’s possible to create pseudo-elements with CSS, however, there is currently no way to provide alternative text if there are images or icons here.',
     'addl. info': ''
@@ -381,10 +383,10 @@ const allCriteria = [
     category: 'Robust',
     subcategory: 'Compatible',
     level: 'A',
+    markdown: ' - Make SVGs accessible to assistive technology',
     DRRSFA: 'Description',
     '*thing*': 'SVG',
     '[more info]()': 'https://a11yengineer.com/thing/image',
-    markdown: ' - Make SVGs accessible to assistive technology',
     notes:
       'SVGs are often used as icons on interactive elements in sites. When this is the case, the SVG should have a title or embedded text (that is visually hidden but available to assistive tech) to ensure that the information of the icon is available programmatically.',
     'addl. info': ''
@@ -396,10 +398,10 @@ const allCriteria = [
     category: 'Robust',
     subcategory: 'Compatible',
     level: 'A',
+    markdown: ' - Create alternate routes for users to access information',
     DRRSFA: '',
     '*thing*': '*',
-    '[more info]()': 'https://webaim.org/standards/wcag/checklist',
-    markdown: ' - Create alternate routes for users to access information',
+    '[more info]()': '',
     notes:
       'It can be difficult to make certain components (like an SVG map) into accessible experiences. In those cases, consider creating an additional screen reader-friendly experience, or at the very least, describe the experience to users.',
     'addl. info': ''
